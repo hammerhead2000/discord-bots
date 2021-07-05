8e658bc0096178fa7b972416b577ae3ae1c129b7
Simon Birtwistle <simon@birtwistle.me.uk>
2021-07-05 23:14:28 +0100

    Bot test 2

--- botregtest.py
+++ botregtest.py
@@ -10,3 +10,5 @@ class TryTesting(TestCase):
             @client.event
             async def on_ready():
                 print('We have logged in as {0.user}'.format(client))
+
+            client.run(TOKEN)
